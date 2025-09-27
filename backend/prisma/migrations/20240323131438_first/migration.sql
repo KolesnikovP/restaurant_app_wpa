db2d87bd4c1a7eee80de9ce61ee97e30c5040f18
petr <ptrklsnkv@gmail.com>
2025-09-26 21:57:00 -0500

    we don't use node js

--- backend/prisma/migrations/20240323131438_first/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Task" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "body" TEXT NOT NULL,
-    "completed" BOOLEAN NOT NULL,
-    "priority" INTEGER NOT NULL
-);
