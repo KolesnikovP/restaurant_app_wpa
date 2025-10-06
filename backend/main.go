8cfc5a536a109fcb8951ff68a414e7df0dcc0f44
petr <ptrklsnkv@gmail.com>
2025-10-06 05:06:01 -0500

    creating a user

--- backend/main.go
+++ backend/main.go
@@ -47,4 +47,22 @@ func main() {
 	}
 
  fmt.Println("SUCCESSFULLY connected to DB!!!! ")
+
+	// create users table
+	createUserQeury := `
+	CREATE TABLE IF NOT EXISTS users (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	email VARCHAR(100) UNIQUE NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	)`
+
+	_, err = db.Exec(createUserQeury)
+	if err != nil {
+		log.Fatal("Error creating table: ", err)
+	}
+// docker exec -it <your-container-name> psql -U postgres -d postgres
+
+	// and then we can see all tables \dt
+	fmt.Println(" users table created successfully!")
 }
