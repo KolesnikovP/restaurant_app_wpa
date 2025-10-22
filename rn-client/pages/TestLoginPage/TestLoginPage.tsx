efd0fcedbc80ba03d94390cae42ec8a1ccdd0ca8
petr <ptrklsnkv@gmail.com>
2025-10-21 22:28:02 -0500

    start from small steps

--- rn-client/pages/TestLoginPage/TestLoginPage.tsx
+++ rn-client/pages/TestLoginPage/TestLoginPage.tsx
@@ -1,12 +1,42 @@
 import { useAuth } from "@/app/providers/auth";
-import { ThemedView } from "@/shared/components/themed-view";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { ThemedText } from "@/shared/components/themed-text";
+import { AppButton } from "@/shared/components/ui/app-button";
+import React, { useEffect, useState } from "react";
+import { View, SafeAreaView, Text, StyleSheet, Button, TextInput } from "react-native";
 
 export default function TestLoginPage() {
   const { signIn } = useAuth()
-  return (
+  const [loginValue, setLoginValue] = useState<string>('')
+  const [passwodValue, setPasswordValue] = useState<string>('')
 
+  const handleSubmit = () => {
+    console.log(loginValue, passwodValue)
+  }
+
+  return (
     <View style={styles.screenStack}>
+      <ThemedText type="title">Login</ThemedText>
+      <TextInput
+        style={styles.input}
+        value={loginValue}
+        onChangeText={setLoginValue}
+        placeholder="login"
+        keyboardType="email-address"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="password"
+        value={passwodValue}
+        onChangeText={setPasswordValue}
+        secureTextEntry
+      />
+
+      <AppButton
+        onPress={handleSubmit}
+        title="login"
+        size="medium"
+      />
+      
       <Text>Login</Text>
       <Button title="Sign in with google" onPress={signIn} />
     </View>
@@ -14,6 +44,13 @@ export default function TestLoginPage() {
 }
 
 const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    paddingHorizontal: 7,
+    width: '70%',
+    borderWidth: 1,
+    margin: 12,
+  },
   screenStack: {
     flex: 1,
     justifyContent: 'center',
